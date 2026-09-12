e0905c5383839986b1f92de186faad863e27763b
yogiindoria <yogeshindoria2000@gmail.com>
2026-09-12 08:49:12 +0000

    Add Ansible playbook notification stage

--- vars/ansibleDeploy.groovy
+++ vars/ansibleDeploy.groovy
@@ -1,6 +1,19 @@
 def call() {
 
+    // Load configuration file
+    def configText = libraryResource('sonarqube.properties')
+    def config = new Properties()
+
+    config.load(new StringReader(configText))
+
+    def slackChannel = config.getProperty('SLACK_CHANNEL_NAME')
+    def environment = config.getProperty('ENVIRONMENT')
+    def codeBasePath = config.getProperty('CODE_BASE_PATH')
+    def actionMessage = config.getProperty('ACTION_MESSAGE')
+    def keepApproval = config.getProperty('KEEP_APPROVAL_STAGE').toBoolean()
+
     stage('Clone') {
+
         echo "Cloning AnsibleSonarQube repository..."
 
         git(
@@ -13,36 +26,43 @@ def call() {
     }
 
     stage('User Approval') {
-        def keepApproval = true
 
         if (keepApproval) {
+
             input(
-                message: 'Do you want to proceed with SonarQube deployment?',
+                message: "Do you want to deploy SonarQube to ${environment}?",
                 ok: 'Proceed'
             )
+
         } else {
+
             echo "Approval stage skipped."
         }
     }
 
     stage('Playbook Execution') {
 
-        echo "Executing SonarQube Ansible playbook..."
+        echo "Environment: ${environment}"
+        echo "Code Base Path: ${codeBasePath}"
 
-        withCredentials([
-            sshUserPrivateKey(
-                credentialsId: 'vm2-ssh-key',
-                keyFileVariable: 'SSH_KEY',
-                usernameVariable: 'SSH_USER'
-            )
-        ]) {
-            sh '''
-                ansible-playbook \
-                -i inventory.ini \
-                -u "$SSH_USER" \
-                --private-key "$SSH_KEY" \
-                site.yml
-            '''
+        dir(codeBasePath) {
+
+            withCredentials([
+                sshUserPrivateKey(
+                    credentialsId: 'vm2-ssh-key',
+                    keyFileVariable: 'SSH_KEY',
+                    usernameVariable: 'SSH_USER'
+                )
+            ]) {
+
+                sh '''
+                    ansible-playbook \
+                    -i inventory.ini \
+                    -u "$SSH_USER" \
+                    --private-key "$SSH_KEY" \
+                    site.yml
+                '''
+            }
         }
 
         echo "Ansible playbook execution completed."
@@ -51,8 +71,8 @@ def call() {
     stage('Notification') {
 
         slackSend(
-            channel: 'build-status',
-            message: 'SonarQube deployment completed successfully.'
+            channel: slackChannel,
+            message: "${actionMessage} Environment: ${environment}"
         )
     }
 }
